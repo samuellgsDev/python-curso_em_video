76f7a93b02f95ce17e594102edf6b03f55c18109
samuellgsDev <samuellgsDev@gmail.com>
2022-02-14 19:12:50 -0300

    exercicio 2 python

---
+++ ex002.py
@@ -0,0 +1,6 @@
+# Make a program that reads a person's name and displays a welcome message.
+# Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de
+# boas-vindas.
+
+name = input(str("what is your name? "))
+print(f'Hello {name}! Welcome to the world of Python!')
