3a8d749dbc87425ab622c52b64c7ef3a7479b755
samuellgsDev <samuellgsDev@gmail.com>
2022-02-14 20:24:13 -0300

    exercicio 009

---
+++ ex009.py
@@ -0,0 +1,15 @@
+# Faça um programa que leia um número Inteiro qualquer e mostre na tela
+# a sua tabuada.
+# Write a program that reads an integer and shows its table.
+
+n = int(input("Enter a number: "))
+print(f"{n} x 1 = {n * 1}")
+print(f"{n} x 2 = {n * 2}")
+print(f"{n} x 3 = {n * 3}")
+print(f"{n} x 4 = {n * 4}")
+print(f"{n} x 5 = {n * 5}")
+print(f"{n} x 6 = {n * 6}")
+print(f"{n} x 7 = {n * 7}")
+print(f"{n} x 8 = {n * 8}")
+print(f"{n} x 9 = {n * 8}")
+print(f"{n} x 10 = {n * 10}")
