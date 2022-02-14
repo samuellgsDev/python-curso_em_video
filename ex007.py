a8460e7427d7880e0eb22310babaf23af6f0f9dd
samuellgsDev <samuellgsDev@gmail.com>
2022-02-14 20:15:11 -0300

    exercicio 007

---
+++ ex007.py
@@ -0,0 +1,8 @@
+# Desenvolva um programa que leia as duas notas de um aluno, calcule
+#  e mostre a sua média.
+# Develop a program that reads the two notes of a student, calculates and shows
+# the student's average.
+n1 = float(input("Enter the first note: "))
+n2 = float(input("Enter the second note: "))
+average = (n1 + n2) / 2
+print(f"The average is: {average}")
