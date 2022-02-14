2f652715babd9d3e8d02dfe622a3c3626f5120a2
samuellgsDev <samuellgsDev@gmail.com>
2022-02-14 19:28:57 -0300

    exercicio 03

---
+++ ex003.py
@@ -0,0 +1,7 @@
+# Create a program that reads two numbers and displays the sum between them.
+# Crie um programa que leia dois números e mostre a soma entre eles.
+
+n1 = int(input('Enter a number: '))
+n2 = int(input('Enter another number: '))
+sum_ = n1 + n2
+print(f'The sum of {n1} and {n2} is {sum_}')
