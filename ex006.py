489b672096e8b834167c346b6905fd672031a391
samuellgsDev <samuellgsDev@gmail.com>
2022-02-14 19:46:00 -0300

    exercicio 06

---
+++ ex006.py
@@ -0,0 +1,9 @@
+# Crie um algoritmo que leia um número e mostre o seu dobro, triplo
+# e raiz quadrada.
+# Create a program that reads a number and shows its double, triple
+#  and square root.
+
+n = int(input('Enter a number: '))
+print(f'The double of {n} is {n*2}')
+print(f'The triple of {n} is {n*3}')
+print(f'The square root of {n} is {n**(1/2)}')
