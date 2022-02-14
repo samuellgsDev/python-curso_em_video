aea372a59762b59c5cf8c60a61600c835a1129c1
samuellgsDev <samuellgsDev@gmail.com>
2022-02-14 19:42:56 -0300

    exercicio 05

---
+++ ex005.py
@@ -0,0 +1,6 @@
+# Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor
+# e seu antecessor.
+# Write a program that reads an integer and shows its successor and predecessor
+n = int(input('Enter a number: '))
+print(f'The successor of {n} is {n+1}')
+print(f'The predecessor of {n} is {n-1}')
