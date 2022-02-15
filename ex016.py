cbc26bb9295ba31898737604069f6dc5425e9157
samuellgsDev <samuellgsDev@gmail.com>
2022-02-15 10:48:32 -0300

    exercicio 016

---
+++ ex016.py
@@ -0,0 +1,8 @@
+# Crie um programa que leia um número Real qualquer pelo teclado
+# e mostre na tela a sua porção Inteira.
+#
+# Create a program that reads any Real number from the keyboard
+# and displays its entire portion on the screen.
+
+n = float(input('Enter a number: '))
+print(f'The integer portion of {n} is {int(n)}')
