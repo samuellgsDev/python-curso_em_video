5602d2de98019e76e79b991ddc4a98cef2e35002
samuellgsDev <samuellgs.dev@gmail.com>
2022-04-19 16:44:37 -0300

    ex096

--- ex096.py
+++ ex096.py
@@ -1,6 +1,6 @@
 # Faça um programa que tenha uma função chamada área(),
 # que receba as dimensões de um terreno retangular (largura e comprimento)
-# e mostre a área do terreno.
+# e mostre a área do terreno em metros.
 def area(largura, comprimento):
     return largura * comprimento
 
