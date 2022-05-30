366e7007946a8527c1abce82be8db8b718d78d1f
samuellgsDev <samuellgs.dev@gmail.com>
2022-05-30 17:40:32 -0300

    modulo do exercicio 107

---
+++ moeda.py
@@ -0,0 +1,17 @@
+# módulo do exercicio 107
+
+
+def aumentar(n):
+    return n + (n * 10 / 100)
+
+
+def diminuir(n):
+    return n - (n * 10 / 100)
+
+
+def dobro(n):
+    return n * 2
+
+
+def metade(n):
+    return n / 2
