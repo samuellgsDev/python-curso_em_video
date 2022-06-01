fa671cd1ad818ecc8788c2e466201fe49c98ebd5
samuellgsDev <samuellgs.dev@gmail.com>
2022-06-01 18:02:36 -0300

    transformando modulos em pacote, ex 111

---
+++ utilidadesCeV/moeda5/__init__.py
@@ -0,0 +1,37 @@
+# módulo do exercicio 110
+
+def aumentar(n, p, formatado=False):
+    res = n + (n * p / 100)
+    return res if formatado is False else moeda(res)
+
+
+def diminuir(n, p, formatado=False):
+    res = n - (n * p / 100)
+    return res if formatado is False else moeda(res)
+
+
+def dobro(n, formatado=False):
+    res = n * 2
+    return res if formatado is False else moeda(res)
+
+
+def metade(n, formatado=False):
+    res = n / 2
+    return res if formatado is False else moeda(res)
+
+
+def moeda(n=0, moeda='R$',):
+    res = f'{moeda}{n:.2f}'.replace('.', ',')
+    return res
+
+
+def resumo(n, a, d):
+    print('-' * 30)
+    print(f'{"RESUMO DO VALOR":^30}')
+    print('-' * 30)
+    print(f'Preço analisado: \t{moeda(n)}')
+    print(f'Dobro do preço: \t{dobro(n, True)}')
+    print(f'Metade do preço: \t{metade(n, True)}')
+    print(f'{a}% de aumento: \t{aumentar(n, a, True)}')
+    print(f'{d}% de redução: \t{diminuir(n, d, True)}')
+    print('-' * 30)
