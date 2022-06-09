3ea3c372d88621dea5630c395e1c85a2085311f9
samuellgsDev <samuellgs.dev@gmail.com>
2022-06-09 01:06:53 -0300

    exercicio 114

---
+++ ex114.py
@@ -0,0 +1,12 @@
+# Crie um código em Python que teste se o site pudim
+# está acessível pelo computador usado.
+import urllib
+import urllib.request
+
+try:
+    site = urllib.request.urlopen('http://www.pudim.com.br')
+except urllib.error.URLError:
+    print('Site fora do ar')
+else:
+    print('Site está acessível')
+    print(site.read())
